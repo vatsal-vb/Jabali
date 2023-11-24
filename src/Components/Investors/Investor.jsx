14424549c30f3de393bc880758cca141fce2aa8c
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 13:41:05 -0800

    Update Investor.jsx

--- src/Components/Investors/Investor.jsx
+++ src/Components/Investors/Investor.jsx
@@ -7,6 +7,10 @@ const Investor = () => {
 
         <h2 className="heading">Investors</h2>
 
+       <p className="additional-paragraph">
+                    We are well funded by top-tier investors from Silicon Valley and Europe.
+                </p>
+
     </div>
   )
 }
