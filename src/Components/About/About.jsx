187a03f62f40e9a730bbe23ba024f7ec0e4ab5a4
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 12:53:58 -0800

    Update About.jsx
    updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,12 +6,10 @@ const About = () => {
             <div className="heading-about">Mission</div>
             <div className="text-container">
                 <p className="paragraph">
-                   Video games is a massive industry with $200B+ in revenues and more than 3.5B players. Over the last decades, the industry has grown and evolved through technological advances, including the shift to mobile, cloud computing, and new business and distribution models. 
-AI is the next technological chapter for the industry. 
+                   Jabali's mission is to democratize creation and bring innovation to video games. We are building a AI-native engine to empower visionary creatives to introduce novel gameplay. Jabali will offer a comprehensive, easy-to-use, engine to create games and interactive experiences, including content, code generation, and personalization.
 
                 </p>
                 <p className="additional-paragraph">
-                  Jabali's mission is to democratize creation and bring innovation to video games. We are creating a AI-native engine, which is not just about cost reduction and efficiency, but also to empower more visionary creatives to introduce novel gameplay. Jabali will offer a comprehensive, easy-to-use, AI engine to create coherent games, including content, code generation, and personalization.
                 </p>
             </div>
         </div>
