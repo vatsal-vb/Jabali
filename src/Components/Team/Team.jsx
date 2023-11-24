293ee94318994e0b0ec96038c2db219839fb877c
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 12:49:54 -0800

    Update Team.jsx
    updates

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Team.css";
+import vatsal from "../../Assets/vatsal.jpeg";
+import arnav from "../../Assets/arnav.jpeg";
 
 const Team = () => {
     const teamMembers = [
@@ -7,6 +9,7 @@ const Team = () => {
             id: 1,
             name: 'Vatsal Bhardwaj',
             role: 'Founder and CEO',
+            img src={logo} 
             image: 'https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80',
         },
         {
@@ -39,13 +42,12 @@ const Team = () => {
         </div>
 
             <div className="team-text">
+                
                 <p className="paragraph">
-                  We are an experienced team with a deep understanding of the video games, cloud services, AR/VR, and AI/ML. 
-                    <br>
                    Jabali’s founder & CEO Vatsal Bhardwaj,  has a unique background and deep domain expertise in video games having served as an CPO at Skillz, GM & Director of Game Tech at AWS, Product lead at Oculus and GM & Head of Studios for Storm8 games. His products have generated billions of dollars in revenue and reached over a billion players. 
 </p>
                         
-                        <p className="additional-paragraph">
+                        <p>
                                Dr. Arnav Jhala, Co-founder has been on the forefront of AI and Video games research for the last 15 years including 100+ publications in AAAI, IEEE and numerous grants from NSF, DARPA and various leading game companies. Dr. Jhala is also the Director of Digital games research initiative and Visual narrative cluster. 
 
 The rest of the founding team includes software engineering and design with an expertise in developing AI/ML, video games, gaming services, and cloud computing. 
