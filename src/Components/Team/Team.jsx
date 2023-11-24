69fa57b81515f658087a1d532f00b0931a55c5a3
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 12:34:10 -0800

    Update Team.jsx
    update names and titles

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -5,21 +5,15 @@ const Team = () => {
     const teamMembers = [
         {
             id: 1,
-            name: 'Louise Donadieu',
-            role: 'Support Consultant',
+            name: 'Vatsal Bhardwaj',
+            role: 'Founder and CEO',
             image: 'https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80',
         },
         {
             id: 2,
-            name: 'Jeff Fisher',
-            role: 'Project Manager',
+            name: 'Arnav Jhala',
+            role: 'Co-Founder and Chief Scientist',
             image: 'https://images.unsplash.com/photo-1514222709107-a180c68d72b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80',
-        },
-        {
-            id: 3,
-            name: 'Tom Lowry',
-            role: 'UI/UX Designer',
-            image: 'https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80',
         },
     ];
 
