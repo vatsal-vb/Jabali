a2fd308684c3c823ac1e24dd98a0aa2c8b89513f
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 09:45:01 -0800

    Update About.jsx
    Updating about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,14 +6,12 @@ const About = () => {
             <div className="heading-about">About Us</div>
             <div className="text-container">
                 <p className="paragraph">
-                    Lorem ipsum dolor sit amet consectetur. Est tristique facilisi urna faucibus orci purus aliquam tempus.
-                    Nunc sit posuere purus et. Rhoncus lacus faucibus porttitor tempus lectus vestibulum neque eget vel. Sed
-                    morbi eu feugiat amet eu elit.
+                   Video games is a massive industry with $200B+ in revenues and more than 3.5B players. Over the last decades, the industry has grown and evolved through technological advances, including the shift to mobile, cloud computing, and new business and distribution models. 
+AI is the next technological chapter for the industry. 
+
                 </p>
                 <p className="additional-paragraph">
-                    Lorem ipsum dolor sit amet consectetur. Est tristique facilisi urna faucibus orci purus aliquam tempus.
-                    Nunc sit posuere purus et. Rhoncus lacus faucibus porttitor tempus lectus vestibulum neque eget vel. Sed
-                    morbi eu feugiat amet eu elit.
+                  Jabali's mission is to democratize creation and bring innovation to video games. We are creating a AI-native engine, which is not just about cost reduction and efficiency, but also to empower more visionary creatives to introduce novel gameplay. Jabali will offer a comprehensive, easy-to-use, AI engine to create coherent games, including content, code generation, and personalization.
                 </p>
             </div>
         </div>
