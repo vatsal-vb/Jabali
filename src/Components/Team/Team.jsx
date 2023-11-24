a227b0b7eb61ee4f369987ad2a2bd944c9bd1a76
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 13:37:36 -0800

    Update Team.jsx
    updates

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -38,8 +38,10 @@ const Team = () => {
             ))}
         </div>
 
-            <div className="team-text">
-                <p className="paragraph">
+
+
+            <div className="text-container">
+                 <p className="paragraph">
                   We are an experienced team with a deep understanding of the video games, cloud services, AR/VR, and AI/ML. 
                     </p>
                 
@@ -48,14 +50,20 @@ const Team = () => {
                     Jabali’s founder & CEO Vatsal Bhardwaj,  has a unique background and deep domain expertise in video games having served as an CPO at Skillz, GM & Director of Game Tech at AWS, Product lead at Oculus and GM & Head of Studios for Storm8 games. His products have generated billions of dollars in revenue and reached over a billion players. 
 
                     </p>
-                        <p className="paragraph">
-                        Dr. Arnav Jhala, Co-founder has been on the forefront of AI and Video games research for the last 15 years including 100+ publications in AAAI, IEEE and numerous grants from NSF, DARPA and various leading game companies. Dr. Jhala is also the Director of Digital games research initiative and Visual narrative cluster. 
-
-                        </p>
+                
 <p className="paragraph">
-The rest of the founding team includes software engineering and design with an expertise in developing AI/ML, video games, gaming services, and cloud computing. 
-        </p>
+                The rest of the founding team includes software engineering and design with an expertise in developing AI/ML, video games, gaming services, and cloud computing. 
+</p>
+    
+                <p className="additional-paragraph">
+                    Dr. Arnav Jhala, Co-founder has been on the forefront of AI and Video games research for the last 15 years including 100+ publications in AAAI, IEEE and numerous grants from NSF, DARPA and various leading game companies. Dr. Jhala is also the Director of Digital games research initiative and Visual narrative cluster. 
+                </p>
             </div>
+
+            
+            
+
+        
 
 
             
