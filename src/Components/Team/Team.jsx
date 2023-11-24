ae38044ea55ea7f021d8d4101d2604e61f4e6ea2
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 13:39:49 -0800

    Update Team.jsx

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -50,10 +50,7 @@ const Team = () => {
                     Jabali’s founder & CEO Vatsal Bhardwaj,  has a unique background and deep domain expertise in video games having served as an CPO at Skillz, GM & Director of Game Tech at AWS, Product lead at Oculus and GM & Head of Studios for Storm8 games. His products have generated billions of dollars in revenue and reached over a billion players. 
 
                     </p>
-                
-<p className="paragraph">
-                The rest of the founding team includes software engineering and design with an expertise in developing AI/ML, video games, gaming services, and cloud computing. 
-</p>
+
     
                 <p className="additional-paragraph">
                     Dr. Arnav Jhala, Co-founder has been on the forefront of AI and Video games research for the last 15 years including 100+ publications in AAAI, IEEE and numerous grants from NSF, DARPA and various leading game companies. Dr. Jhala is also the Director of Digital games research initiative and Visual narrative cluster. 
