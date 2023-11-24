aa47328b6caee24108104251a839cb33f2d4b8ac
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 13:08:26 -0800

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,7 +3,7 @@ import "./About.css";
 const About = () => {
     return (
         <div className="about-container">
-            <div className="heading-about">What we do</div>
+            <div className="heading-about">What We Do</div>
             <div className="text-container">
                 <p className="paragraph">
                    Jabali's mission is to democratize creation and bring innovation to video games. We are building a AI-native engine to empower visionary creatives to introduce novel gameplay. Jabali will offer a comprehensive, easy-to-use, engine to create games and interactive experiences, including content, characters, code generation, and personalization.
