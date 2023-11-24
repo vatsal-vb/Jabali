6b13d593d086e40fa5329851b1b946c0fb462db8
vatsal-vb <135896746+vatsal-vb@users.noreply.github.com>
2023-11-24 13:49:25 -0800

    Update Footer.jsx
    mail icon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,8 +7,9 @@ const Footer = () => {
         <li><a href="mailto:contact@jabali.ai"> contact@jabali.ai </a></li>
       </ul>
       <ul className="footer-icons">
-        <li><a href="https://twitter.com/"><ion-icon name="logo-twitter"></ion-icon></a></li>
-        <li><a href="https://linkedin.com/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+        <li><a href="https://twitter.com/vatsal"><ion-icon name="logo-twitter"></ion-icon></a></li>
+        <li><a href="https://linkedin.com/in/vatsal"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+         <li><a href="mailto:contact@jabali.ai"> <ion-icon name="logo-mail"></ion-icon> </a></li>
       </ul>
     </footer>
   )
